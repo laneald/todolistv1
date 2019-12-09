c413e65dade8d86b2c3e41b6ce7bfbc5b7dc16c4
Laneal Dickerson <laneald@icloud.com>
2019-12-09 16:19:19 -0500

    Changes to todolistv1

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
+//jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('view engine', 'ejs');
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
+    let today = new Date();
+    let currentDay = today.getDay();
 
-    if (currentDay === 6 || currentDay === 0) {
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
 
+    };
+
+    let day = today.toLocaleDateString('en-US', options);
+
+    res.render('list', {
+        listTitle: day,
+        newListItems: items
+    });
+});
+
+app.post('/', (req, res) => {
+
+    let item = req.body.newItem;
+
+    if (req.body.list === 'Work') {
+        workItems.push(item);
+        res.redirect('/work');
     } else {
+        items.push(item);
 
+        res.redirect('/');
     }
 });
+
+app.get('/work', (req, res) => {
+    res.render('list', {
+        listTitle: 'Work List',
+        newListItems: workItems
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
